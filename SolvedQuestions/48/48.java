faff2a3b9a4e326155afee44e745f7ae6f21210f
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-05-12 00:32:56 +0530

    Solved: 47, 48

---
+++ SolvedQuestions/48/48.java
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.isPalindrome(S));
+        
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isPalindrome(String S) {
+        char ch[] = S.toCharArray();
+        int start = 0;
+        int end = ch.length-1;
+        int c = 1;
+        while(start<end){
+            if (ch[start]==ch[end]){
+                start++;
+                end--;
+            }else{
+                c=0;
+                break;
+            }
+        }
+        return c;
+    }
+};
