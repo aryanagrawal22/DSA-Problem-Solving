1439c742b4220d0e1991d6271b031597dd204c28
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-05-16 23:59:13 +0530

    Solved 54

---
+++ SolvedQuestions/54/54.java
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.longestPalin(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static String longestPalin(String str){
+        int n = str.length();
+ 
+    // All subStrings of length 1
+    // are palindromes
+    int maxLength = 1, start = 0;
+ 
+    // Nested loop to mark start and end index
+    for (int i = 0; i < str.length(); i++) {
+        for (int j = i; j < str.length(); j++) {
+            int flag = 1;
+ 
+            // Check palindrome
+            for (int k = 0; k < (j - i + 1) / 2; k++)
+                if (str.charAt(i + k) != str.charAt(j - k))
+                    flag = 0;
+ 
+            // Palindrome
+            if (flag!=0 && (j - i + 1) > maxLength) {
+                start = i;
+                maxLength = j - i + 1;
+            }
+        }
+    }
+ 
+ 
+    // return length of LPS
+    return maxLength;
+        
+    }
+}
