fb95bf54df03ec11091a81a20b3240b7880cb1df
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-05-27 02:19:57 +0530

    Solved: 16

--- SolvedQuestions/10/10.cpp
+++ SolvedQuestions/10/10.cpp
@@ -5,45 +5,44 @@ class Solution
 {
 public:
 
-    int maxFinder(int arr[], int maxJump, int currIndex){
-        int max = -99999;
-        int ind ;
-        for (int i =currIndex+1; i<maxJump; i++){
-            if (arr[i]>=max){
-                max = arr[i];
-                ind = i;
-            }
-        }
-        return ind;
-    }
-
     int minJumps(int arr[], int n)
     {
-        int count = 0;
-        for  (int i = 0; i< n; i++){
-            
-            if(arr[i]==0) {
-                count=-1;
-                break;
-            }
-            
-            int jump = maxFinder(arr, arr[i], i);
-            i = i + jump - 1;
-            // cout<<i+1;
-            
-            count++;
+        if(n<=1){
+            return 0;
+        }
 
-            if(i>=n-2){ break;}
+        if(arr[0]==0){
+            return -1;
+        }
+
+        int steps = arr[0];
+        int maxReach = arr[0];
+        int jumps = 1;
+
+        for(int i = 1; i<n; i++){
+            if(i==n-1){
+                return jumps;
+            }
+
+            maxReach = max(maxReach, i + arr[i]);
+
+            steps--;
+            if(steps==0){
+                jumps++;
+                if(i>=maxReach){
+                    return -1;
+                }
+                steps = maxReach - 1;
+            }
 
         }
-    
-        return count;
+        return -1;
     }
 };
 
 int main()
 {
-    int arr[] = {2, 3, 1, 1, 2, 4, 2, 0, 1, 1};
+    int arr[] = {1, 4, 3, 2, 6, 7};
     int n = sizeof(arr)/sizeof(arr[0]);
     Solution obj;
     cout << obj.minJumps(arr, n) << endl;
