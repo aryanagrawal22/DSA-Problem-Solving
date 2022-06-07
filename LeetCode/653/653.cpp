ff07ee5f9c33c2cdee6ad009209e209f666a1fa4
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-06-08 02:21:02 +0530

    Solved: LC653

---
+++ LeetCode/653/653.cpp
@@ -0,0 +1,28 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+    
+//     unordered_set <int> us;
+    
+//     bool findTarget(TreeNode* root, int k) {
+        
+//         if(!root) return false;
+        
+//         if(us.find(k-(root->val)) != us.end()) return true;
+        
+//         us.insert(root->val);
+        
+//         return (findTarget(root->left, k) || findTarget(root->right, k));
+        
+//     }
+// };
