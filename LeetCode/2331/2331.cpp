76e91be956eb2c52b0e50aaeedec4231308138d1
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-07-10 00:13:44 +0530

    Solved: LC1696 LC2331

---
+++ LeetCode/2331/2331.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        
+        if(!root) return true;
+        
+        bool ans1 = evaluateTree(root->left);
+        bool ans2 = evaluateTree(root->right);
+        
+        if(root->val==0 || root->val==1) return root->val;
+        
+        else if(root->val==2) return (ans1 || ans2);
+        
+        else return (ans1 && ans2);
+        
+    }
+};
