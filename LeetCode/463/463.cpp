cc80f8cd80bace06878bef756f21bd87e527c96c
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-09-08 01:26:59 +0530

    Solved: LC338 LC463 LC647

---
+++ LeetCode/463/463.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int ans = 0;
+        for (int i= 0; i<grid.size(); i++){
+            for(int j =0; j<grid[0].size(); j++){
+                if(grid[i][j]){
+                    if(j-1<0 || !grid[i][j-1])ans++;
+                    if(j+1>=grid[0].size() || !grid[i][j+1])ans++;
+                    if(i-1<0 || !grid[i-1][j])ans++;
+                    if(i+1>=grid.size() || !grid[i+1][j])ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
